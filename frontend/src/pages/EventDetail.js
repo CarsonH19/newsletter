694fce4c5e9871ddf8d8870ed5e4491b69fef589
CarsonH19 <questline.carson@gmail.com>
2024-04-07 19:42:55 +0900

    feat: EventsRootLayout

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,5 +1,13 @@
+import { useParams } from "react-router-dom";
+
 function EventDetailPage() {
-  return <h1>EventDetailPage</h1>
+  const params = useParams();
+  return (
+    <>
+      <h1>EventDetailPage</h1>
+      <p>{params.eventId}</p>
+    </>
+  );
 }
 
 export default EventDetailPage;
